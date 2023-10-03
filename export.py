01a37149c50775abca9e9bbafa2868e588580a6b
iPaulTech <79581474+iPaulTech@users.noreply.github.com>
2023-10-03 13:45:19 +0200

    Changed org output to my needs / Code refectoring

--- export.py
+++ export.py
@@ -12,9 +12,14 @@ import os
 
 state_file_name = '.last_export.orgexport'
 
-def main():
-    
+#define a function to build the output text
+outputtext = ""
+def addoutput(text):
+    global outputtext
+    outputtext = outputtext + text
+    return outputtext
 
+def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('inputfile')
 
@@ -35,6 +40,7 @@ def main():
     with open(args.inputfile, 'r') as inputfile:
         text = inputfile.read()
 
+    #Value extraction handling
     note_sep = '''
 -----------------------------------
 
@@ -44,20 +50,30 @@ def main():
 
     note_re = re.compile(r'\n'.join([
         r'(?P<title>.+)'
-        , r'((?P<type>Lesezeichen|Markierung|Notiz)[  ]+auf Seite[  ]+(?P<page>\d+): )'
+        , r'((?P<type>Lesezeichen|Markierung|Notiz)[  ]+auf Seite[  ]+(?P<page>.+): )'
         #note is optional
         + r'''((?P<note>(?:.|\n)*)
 )?''' + r'"(?P<quote>(?:.|\n)*)"'
         , r'''Hinzugefügt am (?P<day>\d{2}).(?P<month>\d{2}).(?P<year>\d{4}) \| (?P<hour>\d{1,2}):(?P<minute>\d{2})
 ''']))
-
+    #define note types (from tolino notes.txt)
     types = dict(
-        Markierung='highlighted'
-        , Notiz='note'
-        , Lesezeichen='marker'
+        Markierung='markierung'
+        , Notiz='notiz'
+        , Lesezeichen='lesezeichen'
         )
 
-    dont_export = ['marker']
+    #Init a Note class (for storing data)
+    class Note:
+        def __init__(self, book_title, note_type, note_page, note_quote, note_date, note_title):
+            self.book_title = book_title
+            self.note_type = note_type
+            self.note_page = note_page
+            self.note_quote = note_quote
+            self.note_date = note_date
+            self.note_title = note_title
+    #Init a list for note class objects
+    store_notes = []
 
     for note in notes:
         if not note:
@@ -67,22 +83,23 @@ def main():
 
         assert(m)
 
+        #Date handling
         d = {}
         for k in ('year', 'month', 'day', 'hour', 'minute'):
             d[k] = int(m.group(k))
 
         created = datetime(**d)
         
+        #Check if note isn't exported yet
         if created < last_export:
             continue
         
         book = m.group('title')
         page = m.group('page')
         typ = types[m.group('type')]
-        if typ in dont_export:
-            continue
         quote = m.group('quote')
 
+        #Make the title beautiful for org :)
         title = quote
         title = org.wrap(title, '"')
         if typ == 'note':
@@ -97,18 +114,47 @@ def main():
             title = ' '.join(title_words[:n])
             title += ' […]'
 
+        #Store values in the store_notes object list
+        store_notes.append(Note(book, typ, page, quote, created, title))
 
-        content += '\n#+begin_quote\n' + quote + '\n#+end_quote\n'
-
-        content += '\nFrom "' + book + '"' + ', p.' + page + '\n'
-        
-        s = org.headline(title, content, created, tags=[typ])
-        print(s)
-        print('\n')
-
+    #Store new last write state permanently
     last_export_lines.append(datetime.now().strftime(org.date_format))
     with open(state_file, 'w+') as f:
         f.write('\n'.join(last_export_lines))
+
+    #Looping trough stored notes and create a list with all possible book titles
+    store_booktitles = []
+    for x in store_notes:
+        if not x.book_title in store_booktitles:
+            store_booktitles.append(x.book_title)
+
+    #Create org output for every book
+    for x in store_booktitles:
+        #Booktitle (/Bookname)
+        addoutput("* " + x + "\n")
+
+        #Add a boolean for getting first created propertie for book heading only once
+        first_item = True
+        #For every stored note check if it is for the selected book
+        for note_item in store_notes:
+            if note_item.book_title == x:
+                if first_item:
+                    first_item = False
+                    addoutput(org.datepropertie(note_item.note_date))
+
+                #Create note text (/Bookname/Note)
+                addoutput("** S. " + note_item.note_page + " " + note_item.note_title + ' :' + note_item.note_type + ":" "\n")
+                #Datepropertie for note
+                addoutput(org.datepropertie(note_item.note_date))
+                #Quote
+                addoutput("#+begin_quote\n")
+                addoutput(note_item.note_quote + "\n")
+                addoutput("#+end_quote\n")
+                #Reference text
+                addoutput('Von "' + note_item.book_title + '", S. ' + note_item.note_page)
+                addoutput("\n")
+    #Print the result
+    print(outputtext)
 
 if __name__ == '__main__':
     main()
